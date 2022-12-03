07afb501e6736dcb82bb3061653c7829ea019786
JoelKanyi <joelkanyi98@gmail.com>
2022-12-03 18:47:20 +0300

    setup spotless and ktlint

--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Joel Kanyi.
+ * Copyright $YEAR Joel Kanyi.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,18 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.kanyideveloper.mealtime.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val MainOrange = Color(0xFFfa4a0c)
-val LightGrey = Color(0xFFe8e8e8)
-val MainGrey = Color(0xFFcacccf)
-val DarkGrey = Color(0xFFa0a4a8)
-val MyPink = Color(0xFFff546c)
-val MyLightPink = Color(0xFFffe4e8)
-val MyGreen = Color(0xFF1ad391)
-val MyLightGreen = Color(0xFFe1ecdc)
-val MyLightOrange = Color(0xFFf8e5de)
-val MyLightBlue = Color(0xFFd3e5f3)
-val MyLightYellow = Color(0xFFfef6d4)
