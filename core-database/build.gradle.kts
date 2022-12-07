0d1d0a1fad2cb799a6f47d4be4c23947d33d1373
JoelKanyi <joelkanyi98@gmail.com>
2022-12-07 17:50:40 +0300

    CORE-DATABASE - create the module

---
+++ core-database/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id(Plugins.androidLibrary)
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("org.jetbrains.kotlin.android")
+}
+
+apply {
+    from("$rootDir/base-module.gradle")
+}
+
+android {
+    compileSdk = AndroidConfig.compileSDK
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSDK
+        targetSdk = AndroidConfig.targetSDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.composeCompiler
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(project(Modules.core))
+}
