db3e95eda0978d79b14a0ed6d0f11fc95371dcfd
JoelKanyi <joelkanyi98@gmail.com>
2022-12-29 13:46:11 +0300

    FAVORITE - some minor edits on the fav screen

--- feature/favorites/src/main/java/com/kanyideveloper/favorites/presentation/favorites/presentation/FavoritesScreen.kt
+++ feature/favorites/src/main/java/com/kanyideveloper/favorites/presentation/favorites/presentation/FavoritesScreen.kt
@@ -16,6 +16,7 @@
 package com.kanyideveloper.favorites.presentation.favorites.presentation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -30,11 +31,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -42,10 +44,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -138,62 +139,55 @@ fun FoodItem(
             .height(130.dp)
             .padding(horizontal = 8.dp, vertical = 5.dp),
         shape = Shapes.large,
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
         onClick = {
             onClick(favorite.id, favorite.onlineMealId, favorite.localMealId, favorite.isOnline)
         }
     ) {
-        Row(Modifier.fillMaxWidth()) {
-            Image(
-                modifier = Modifier
-                    .fillMaxWidth(0.4f)
-                    .fillMaxHeight(),
-                contentDescription = null,
-                painter = rememberAsyncImagePainter(
-                    ImageRequest.Builder(LocalContext.current)
-                        .data(data = favorite.mealImageUrl)
-                        .apply(block = fun ImageRequest.Builder.() {
-                            placeholder(R.drawable.food_loading)
-                        }).build()
-                ),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Column(
-                Modifier
-                    .fillMaxWidth(),
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = favorite.mealName,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold
+        Box(modifier = Modifier.fillMaxSize()) {
+            Row(Modifier.fillMaxWidth()) {
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth(0.4f)
+                        .fillMaxHeight(),
+                    contentDescription = null,
+                    painter = rememberAsyncImagePainter(
+                        ImageRequest.Builder(LocalContext.current)
+                            .data(data = favorite.mealImageUrl)
+                            .apply(block = fun ImageRequest.Builder.() {
+                                placeholder(R.drawable.food_loading)
+                            }).build()
+                    ),
+                    contentScale = ContentScale.Crop
                 )
-                Spacer(modifier = Modifier.height(12.dp))
+                Spacer(modifier = Modifier.width(8.dp))
 
-                Row(
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.CenterVertically
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    IconButton(onClick = {
-                        onFavoriteClick(favorite)
-                    }) {
-                        Icon(
-                            modifier = Modifier
-                                .size(24.dp),
-                            painter = painterResource(
-                                id = if (favorite.isFavorite) {
-                                    R.drawable.filled_favorite
-                                } else {
-                                    R.drawable.unfilled_favorite
-                                }
-                            ),
-                            contentDescription = null
-                        )
-                    }
+                    Text(
+                        text = favorite.mealName,
+                        style = MaterialTheme.typography.titleMedium
+                    )
                 }
             }
+
+            Icon(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.BottomEnd)
+                    .size(32.dp)
+                    .padding(0.dp)
+                    .clickable {
+                        onFavoriteClick(favorite)
+                    },
+                imageVector = Icons.Filled.Favorite,
+                contentDescription = null,
+                tint = Color(0xFFfa4a0c)
+            )
         }
     }
 }
