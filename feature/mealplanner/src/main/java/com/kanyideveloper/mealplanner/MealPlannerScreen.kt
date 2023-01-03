bbadaa328e296d0f0d40570011406a40c1d91f86
JoelKanyi <joelkanyi98@gmail.com>
2023-01-03 12:35:49 +0300

    meal planner setup

--- feature/mealplanner/src/main/java/com/kanyideveloper/mealplanner/MealPlannerScreen.kt
+++ feature/mealplanner/src/main/java/com/kanyideveloper/mealplanner/MealPlannerScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -37,6 +38,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -45,6 +47,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -55,6 +61,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.kanyideveloper.compose_ui.components.StandardToolbar
@@ -76,9 +83,14 @@ interface MealPlannerNavigator {
 @Destination
 @Composable
 fun MealPlannerScreen(
-    navigator: MealPlannerNavigator
+    navigator: MealPlannerNavigator,
+    viewModel: MealPlannerViewModel = hiltViewModel()
 ) {
     val hasMealPlan = true
+    var shouldShowMealsDialog by remember {
+        mutableStateOf(false)
+    }
+
     Column(Modifier.fillMaxSize()) {
         StandardToolbar(
             navigate = {
@@ -91,6 +103,15 @@ fun MealPlannerScreen(
             navActions = {
             }
         )
+
+        if (shouldShowMealsDialog) {
+            SelectMealDialog(
+                mealType = viewModel.mealType.value,
+                onDismiss = {
+                    shouldShowMealsDialog = !shouldShowMealsDialog
+                }
+            )
+        }
 
         Box(modifier = Modifier.fillMaxSize()) {
             if (!hasMealPlan) {
@@ -125,7 +146,13 @@ fun MealPlannerScreen(
                     }
 
                     items(mealTypes) { type ->
-                        MealPlanItem(mealType = type)
+                        MealPlanItem(
+                            mealType = type,
+                            onClickAdd = { mealType ->
+                                viewModel.setMealTypeState(mealType)
+                                shouldShowMealsDialog = !shouldShowMealsDialog
+                            }
+                        )
                     }
                 }
             }
@@ -182,7 +209,8 @@ data class MealType(
 
 @Composable
 fun MealPlanItem(
-    mealType: MealType
+    mealType: MealType,
+    onClickAdd: (String) -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -211,6 +239,7 @@ fun MealPlanItem(
                     style = MaterialTheme.typography.titleMedium
                 )
                 IconButton(onClick = {
+                    onClickAdd(mealType.name)
                 }) {
                     Icon(
                         modifier = Modifier.size(32.dp),
@@ -367,4 +396,29 @@ fun PlanMealItem(
             }
         }
     }
+}
+
+@Composable
+fun SelectMealDialog(
+    onDismiss: () -> Unit,
+    mealType: String
+) {
+    AlertDialog(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(.9f),
+        onDismissRequest = { onDismiss() },
+        title = {
+            Text(
+                text = mealType,
+                style = MaterialTheme.typography.titleLarge
+            )
+        },
+        text = {
+            Column(verticalArrangement = Arrangement.spacedBy(6.dp)) {
+            }
+        },
+        confirmButton = {
+        }
+    )
 }
