16d3d87bb4b8e3441f13abbeb3e4031f202ae10d
JoelKanyi <joelkanyi98@gmail.com>
2023-03-19 10:29:29 +0300

    backup my meal and favs - revamp

--- core-database/src/androidTest/java/com/kanyideveloper/core_database/LivedataTestUtil.kt
+++ core-database/src/androidTest/java/com/kanyideveloper/core_database/LivedataTestUtil.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("UNCHECKED_CAST")
+
 package com.kanyideveloper.core_database
 
 import androidx.lifecycle.LiveData
